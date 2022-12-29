40dd3f09346ba94594a76fe2f9156bcfd884f124
musharraf ansari <musharrafansari>
2022-12-29 11:40:55 +0530

    done

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,19 +1,10 @@
-// import React,{useState} from 'react'
+
 import './Navbar.css';
 import Logo from '../images/logo.png'
-// import {FaBars, FaTimes} from 'react-icons/fa'
+
 
 function Navbar() {
-//   const [click, setClick] = useState(false)
-//   const handleClick=() => setClick(!click)
-//   const[color, setColor] = useState(false)
-// const changeColor =()=>{
-//   if(window.scrollY >=100){
-//     setColor(true);
-//   }else{
-//     setColor(false)
-//   }
-// }
+
 
 
   return (
